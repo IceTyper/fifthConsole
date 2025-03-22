328dcbfc59e3394495377371ee322253f8e18d00
fmusa <bilayn543@gmail.com>
2025-03-23 00:49:52 +0300

    Started doing CollectionManager 22.03.2025

--- src/Managers/CollectionManager.java
+++ src/Managers/CollectionManager.java
@@ -1,10 +1,17 @@
 package Managers;
 
+import models.SpaceMarine;
+
+import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Deque;
 
 public class CollectionManager<T> {
-    private Deque<T> collection;
+    private Deque<T> collection = new ArrayDeque<>();
+
+    public Deque<T> getCollection() {return collection;}
+
+    public void setCollection(Deque<T> collection) {this.collection = collection;}
 
 
 }
