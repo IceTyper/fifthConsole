3a1962174f3b04c3d3607c9b0906d8b9de418a10
fmusa <bilayn543@gmail.com>
2025-04-14 23:54:28 +0300

    "remove_by_id" is done

--- src/commands/RemoveById.java
+++ src/commands/RemoveById.java
@@ -1,7 +1,9 @@
 package commands;
 
+import important.CollectionManager;
 import important.Core;
 import interfaces.Command;
+import models.SpaceMarine;
 
 public class RemoveById implements Command {
     @Override
@@ -11,6 +13,30 @@ public class RemoveById implements Command {
 
     @Override
     public void execute(Core core, String[] args) {
-        System.out.println("Remove by id");
+        if (args.length <= 1) {
+            System.out.println("ID не получен");
+        } else {
+            try {
+                Long id = Long.parseLong(args[1]);
+                boolean flag = false;
+                CollectionManager<SpaceMarine> cManager = core.getCollectionManager();
+                for (SpaceMarine spaceMarine : cManager.getCollection()) {
+                    if (spaceMarine.getId().equals(id)) {
+                        flag = true;
+                        cManager.getCollection().remove(spaceMarine);
+                        cManager.sortCollection();
+                        break;
+                    }
+                }
+                if (flag) {
+                    System.out.println("Элемент успешно удалён");
+                } else {
+                    System.out.println("Элемент не найден");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Id у вас не числовое какое-то");
+            }
+        }
+
     }
 }
