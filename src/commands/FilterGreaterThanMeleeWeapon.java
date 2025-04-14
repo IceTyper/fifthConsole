5dde5ed8da7536eb71f94e6e9a580ca740193f2b
fmusa <bilayn543@gmail.com>
2025-04-14 23:27:23 +0300

    "filter_greater_than_melee_weapon" is done... probably

--- src/commands/FilterGreaterThanMeleeWeapon.java
+++ src/commands/FilterGreaterThanMeleeWeapon.java
@@ -30,15 +30,15 @@ public class FilterGreaterThanMeleeWeapon implements Command {
             if (!flag) {
                 System.out.println("Ни один подходящий элемент не найден");
             }
-        } catch (IllegalArgumentException e) {
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Холодное оружие не введено");
+        } catch (Exception e) {
             System.out.println(Arrays.toString(args));
             System.out.println("Холодное оружие введено неверно или его вообще нет, попробуйте ещё раз");
             System.out.println("Доступные варианты: ");
             for (MeleeWeapon meleeWeapon : MeleeWeapon.values()) {
                 System.out.print(meleeWeapon + " ");
             }
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Холодное оружие не введено");
         }
     }
 }
