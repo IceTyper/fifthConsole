c11bddb0a2dce51b311e0751afa44626402b7c0b
fmusa <bilayn543@gmail.com>
2025-04-22 20:19:04 +0300

    "RedundantArguments" is slightly changed

--- src/main/java/org/example/Exceptions/RedundantArguments.java
+++ src/main/java/org/example/Exceptions/RedundantArguments.java
@@ -2,7 +2,5 @@ package org.example.Exceptions;
 
 public class RedundantArguments extends RuntimeException {
     public RedundantArguments() { }
-    public String printProblem(String[] args) {return "Что за бред вы вводите, "  +
-            "я даже чисто из принципа это обрабатывать не буду, " +
-            "даже если команду '" + args[0] + "' вы написали правильно.";}
+    public String printProblem(String[] args) {return "Символов после '" + args[0] + "' слишком много.";}
 }
