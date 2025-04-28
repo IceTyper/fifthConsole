3c1bc2cc3b2e2c1939ab3f24f77d24e94e9d0788
fmusa <bilayn543@gmail.com>
2025-04-28 22:09:40 +0300

    Improved "Save" - now it can save to the chosen file

--- src/main/java/org/example/commands/Save.java
+++ src/main/java/org/example/commands/Save.java
@@ -1,26 +1,29 @@
 package org.example.commands;
+
 import org.example.Exceptions.RedundantArguments;
 import org.example.important.Core;
 import org.example.interfaces.Command;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.Writer;
 
 public class Save implements Command {
     @Override
     public String getDescription() {
-        return "save - сохранение коллекции в файл";
+        return "save {file_name} - сохранение коллекции в файл (имя файла вводить необязательно)";
     }
 
     @Override
     public void execute(Core core, String[] args) {
         try {
-            if (args.length > 1) {
+            if (args.length > 2) {
                 throw new RedundantArguments();
             }
-            core.getFileManager().saveToFile(new File("C:/Users/fmusa/IdeaProjects/fifthConsole/src/main/resources/collection.json"), core.getCollectionManager());
+            if (args.length == 2 && args[1].endsWith(".json")) {
+                core.getFileManager().saveToFile(new File("C:/Users/fmusa/IdeaProjects/fifthConsole/src/main/resources/" + args[1]), core.getCollectionManager());
+            } else {
+                core.getFileManager().saveToFile(new File("C:/Users/fmusa/IdeaProjects/fifthConsole/src/main/resources/collection.json"), core.getCollectionManager());
+            }
         } catch (RedundantArguments e) {
             System.out.println(e.printProblem(args));
         } catch (IOException e) {
