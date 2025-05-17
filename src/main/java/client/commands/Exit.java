40cf56a5868d9c26783bc75d52d355ae2509272c
fmusa <bilayn543@gmail.com>
2025-05-17 23:01:21 +0300

    Primitive version of "Exit" on client

---
+++ src/main/java/client/commands/Exit.java
@@ -0,0 +1,25 @@
+package client.commands;
+
+
+import client.Client;
+import client.exceptions.RedundantArgumentsException;
+
+public class Exit implements Command {
+    @Override
+    public String getDescription() {
+        return "exit - завершение программы без сохранения в файл";
+    }
+
+    @Override
+    public void execute(String[] args) {
+        try {
+            if (args.length > 1) {
+                throw new RedundantArgumentsException();
+            }
+            Client.endClient();
+            System.out.println("Прощай, юзер!");
+        } catch (RedundantArgumentsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
