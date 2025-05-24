863088c938cf59127d8a94c45b4bf4709d4d75a3
fmusa <bilayn543@gmail.com>
2025-05-24 19:45:30 +0300

    Made Message.java serializable, created class Serializator

--- client/src/main/java/connectionchamber/Message.java
+++ client/src/main/java/connectionchamber/Message.java
@@ -1,6 +1,12 @@
 package connectionchamber;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public record Message(String commandName, Object[] args) implements Serializable {
+    public String[] getMessage() {
+        return Arrays.stream(args).
+                map(Object::toString).
+                toArray(String[]::new);
+    }
 }
