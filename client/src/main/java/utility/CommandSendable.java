430c48692de813c62c63aaa21c81dac06347c9eb
fmusa <bilayn543@gmail.com>
2025-06-03 19:23:39 +0300

    deleted senders, moved txt files

--- client/src/main/java/utility/CommandSendable.java
+++
@@ -1,9 +0,0 @@
-package utility;
-
-import commands.Command;
-import connectionchamber.ClientConnectable;
-import connectionchamber.Message;
-
-public interface CommandSendable {
-    void sendCommandToServer(Message msg, ClientConnectable client);
-}
