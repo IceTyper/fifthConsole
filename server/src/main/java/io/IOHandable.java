7ac20c06fe872598e5e8a3a905b47d5004ba5d89
fmusa <bilayn543@gmail.com>
2025-06-11 15:22:12 +0300

    Created IOHandler in server

--- server/src/main/java/io/IOHandable.java
+++ server/src/main/java/io/IOHandable.java
@@ -1,11 +1,12 @@
 package io;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 //интерфейс для классов, отвечающих за ввод-вывод в консоли и файле
 public interface IOHandable {
-    String readLine() throws IOException;
+    String readLine() throws NoSuchElementException;
 
     boolean hasNextLine();
 
---
+++ server/src/main/java/io/IOHandler.java
@@ -0,0 +1,40 @@
+package io;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class IOHandler implements IOHandable{
+    private static Scanner defaultScanner = new Scanner(System.in);
+    private static Scanner fileScanner = null;
+
+    @Override
+    public String readLine() throws NoSuchElementException {
+        return (fileScanner != null) ? fileScanner.nextLine() : defaultScanner.nextLine();
+    }
+
+    @Override
+    public boolean hasNextLine() {
+        return (fileScanner != null) ? fileScanner.hasNextLine() : defaultScanner.hasNextLine();;
+    }
+
+    @Override
+    public void selectScanner(Scanner scanner) {
+        fileScanner = scanner;
+    }
+
+    @Override
+    public void selectConsoleScanner() {
+        fileScanner = null;
+    }
+
+    @Override
+    public String readConsoleLine() {
+        defaultScanner.nextLine();
+    }
+
+    @Override
+    public boolean isReadingFromFile() {
+        return fileScanner != null;
+    }
+}
