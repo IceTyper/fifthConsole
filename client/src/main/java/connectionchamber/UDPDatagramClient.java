2bea59a4079cb2603918a48f67e3607155ac41f6
fmusa <bilayn543@gmail.com>
2025-06-20 14:26:34 +0300

    now client awaits an answer from server for 30 seconds

--- client/src/main/java/connectionchamber/UDPDatagramClient.java
+++ client/src/main/java/connectionchamber/UDPDatagramClient.java
@@ -9,21 +9,21 @@ import java.net.InetAddress;
 import java.net.SocketException;
 
 public class UDPDatagramClient implements Connectable {
+    private static User user;
     private final DatagramSocket socket;
     private InetAddress address;
     private int port;
-    private static User user;
 
-    public static void setUser(User user) {
-        UDPDatagramClient.user = user;
+    public UDPDatagramClient() throws SocketException {
+        socket = new DatagramSocket();
     }
 
     public static User getUser() {
         return user;
     }
 
-    public UDPDatagramClient() throws SocketException {
-        socket = new DatagramSocket();
+    public static void setUser(User user) {
+        UDPDatagramClient.user = user;
     }
 
     @Override
@@ -42,14 +42,23 @@ public class UDPDatagramClient implements Connectable {
     @Override
     public byte[] receive() throws IOException {
         //System.out.println("Ждём ответ от сервера...");
-        byte[] buffer = new byte[4096];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
-        int length = packet.getLength();
-        byte[] received = new byte[length];
-        System.arraycopy(buffer, 0, received, 0, length);
-        //System.out.println("Ответ получен!");
-        return received;
+        try {
+            socket.setSoTimeout(30 * 1000);
+            byte[] buffer = new byte[4096];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            System.out.println("Ждём ответ от сервера...");
+            socket.receive(packet);
+            int length = packet.getLength();
+            byte[] received = new byte[length];
+            System.arraycopy(buffer, 0, received, 0, length);
+            //System.out.println("Ответ получен!");
+            return received;
+        } catch (IOException e) {
+            throw new IOException("превышено время ожидано от серера (30 секунд)");
+        }
+        finally {
+            socket.setSoTimeout(0);
+        }
     }
 
     @Override
